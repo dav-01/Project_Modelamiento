7e97f2320bd83bcd839dca143af78a6af775cc5a
David Cabrera <cabrerabuelvasd@gmail.com>
2025-04-29 19:29:50 -0500

    Se agrego el metodo IntLieal funcional en el software

---
+++ backend/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const fs = require('fs');
+const { exec } = require('child_process');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/calcular', (req, res) => {
+  const { metodo, x, X, Y } = req.body;
+
+  const tempScriptPath = path.join(__dirname, 'temp_script.m');
+  const metodoPath = path.join(__dirname, 'octave', `${metodo}.m`);
+
+  const X_str = `[${X.join(',')}]`;
+  const Y_str = `[${Y.join(',')}]`;
+
+  // Crear contenido para el script temporal
+  const scriptContent = `
+  addpath(genpath("${path.dirname(metodoPath).replace(/\\/g, "/")}"));
+  x = ${x};
+  X = ${X_str};
+  Y = ${Y_str};
+  resultado = ${metodo}(x, X, Y);
+  disp("RESULTADO:");
+  disp(resultado);
+`;
+
+
+
+  fs.writeFileSync(tempScriptPath, scriptContent);
+
+  exec(`octave-cli --quiet "${tempScriptPath}"`, (error, stdout, stderr) => {
+    if (error) {
+      console.error('Error:', error.message);
+      return res.status(500).json({ error: 'Error al ejecutar Octave' });
+    }
+
+    const match = stdout.match(/RESULTADO:\s+([\s\S]*)/);
+    const resultado = match ? match[1].trim() : stdout.trim();
+
+    res.json({ resultado });
+    fs.unlinkSync(tempScriptPath); // elimina script temporal
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
