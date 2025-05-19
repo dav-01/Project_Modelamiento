e839f1d47832dd92dd733007ceaf6b20c1d5e494
Miguel A <madape05@gmail.com>
2025-05-18 23:12:23 -0500

    Se cambió el diseño de la página

--- backend/octave/mincuadlin.m
+++ backend/octave/mincuadlin.m
@@ -5,11 +5,11 @@ function [m,b] = mincuadlin(X, Y)
   B = zeros(2,1);
 
   for i = 1:n
-    A(1,1) += X(i)^2;
-    A(1,2) += X(i);
-    A(2,1) += X(i);
-    B(1,1) += X(i) * Y(i);
-    B(2,1) += Y(i);
+    A(1,1) = A(1,1) + X(i)^2;
+    A(1,2) = A(1,2) + X(i);
+    A(2,1) = A(2,1) + X(i);
+    B(1,1) = B(1,1) + X(i)*Y(i);
+    B(2,1) = B(2,1) + Y(i);
   endfor
 
   A(2,2) = n;
@@ -17,6 +17,4 @@ function [m,b] = mincuadlin(X, Y)
 
   m = sol(1,1);
   b = sol(2,1);
-
-  disp([m, b]);
 end
---
+++ backend/temp_script_8c6c5945-4663-42c5-b8cb-0fd516c87cfe.m
@@ -0,0 +1,8 @@
+
+        addpath(genpath("C:/xampp/htdocs/Project_Modelamiento/backend/octave"));
+        x = 4.5;
+        X = [0];
+        Y = [0];
+        resultado = IntLineal(x, X, Y);
+        disp(resultado);
+      
